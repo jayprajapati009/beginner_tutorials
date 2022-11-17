65938063659f7d616f094ad2417a507cb63817b3
jayprajapati009 <jayprajapati9900@gmail.com>
2022-11-16 22:47:30 -0500

    added a service in the talker node

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -17,10 +17,14 @@
 #include <memory>
 #include <string>
 
+#include "cpp_pubsub/srv/modify_msg.hpp"
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 
+using std::placeholders::_1;
 using namespace std::chrono_literals;
+
+using sharedFuture = rclcpp::Client<cpp_pubsub::srv::ModifyMsg>::SharedFuture;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
@@ -31,21 +35,65 @@ class MinimalPublisher : public rclcpp::Node {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
         500ms, std::bind(&MinimalPublisher::timer_callback, this));
+
+    // changes Required
+    client = this->create_client<cpp_pubsub::srv::ModifyMsg>("modify_msg");
+    RCLCPP_DEBUG(this->get_logger(), "Client created");
+    while (!client->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_FATAL(rclcpp::get_logger("rclcpp"),"Interrupted");
+        exit(EXIT_FAILURE);
+      }
+      RCLCPP_WARN(rclcpp::get_logger("rclcpp"),
+                  "Service unavailable");
+    }
   }
+  // changes required
 
  private:
+  std::string Message;
+  rclcpp::Client<cpp_pubsub::srv::ModifyMsg>::SharedPtr client;
+
   void timer_callback() {
+    RCLCPP_INFO_STREAM_ONCE(this->get_logger(), "Node setup");
     auto message = std_msgs::msg::String();
     message.data = "The count is " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
+    if (count_ % 10 == 0) {
+      call_service();
+    }
+    auto steady_clock = rclcpp::Clock();
+    RCLCPP_DEBUG_STREAM_THROTTLE(this->get_logger(), steady_clock, 10000,
+                                 "Node running successfully");
   }
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
+
+  // changes Required
+  int call_service() {
+    auto request = std::make_shared<cpp_pubsub::srv::ModifyMsg::Request>();
+    request->a = "String1";
+    request->b = " String2";
+    RCLCPP_INFO(this->get_logger(), "Calling Service to Modify string");
+    auto callbackPtr = std::bind(&MinimalPublisher::response_callback, this, _1);
+    client->async_send_request(request, callbackPtr);
+    return 1;
+  }
+  // changes Required  
+
+  // changes Required
+  void response_callback(sharedFuture future) {
+    // Process the response
+    RCLCPP_INFO(this->get_logger(), "Got String: %s", future.get()->c.c_str());
+    Message = future.get()->c.c_str();
+  }
+  // changes Required4
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
