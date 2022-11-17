53f68deda0be31c2093d8d0bc786060c2d74b226
jayprajapati009 <jayprajapati9900@gmail.com>
2022-11-16 23:01:41 -0500

    Google Style C++ Formatting

--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -32,14 +32,14 @@ using std::placeholders::_1;
 
 /**
  * @brief Subscriber class
- * 
+ *
  */
 class MinimalSubscriber : public rclcpp::Node {
  public:
   /**
-    * @brief Construct a new Minimal Subscriber object
-    * 
-    */
+   * @brief Construct a new Minimal Subscriber object
+   *
+   */
   MinimalSubscriber() : Node("minimal_subscriber") {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
         "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
@@ -47,9 +47,10 @@ class MinimalSubscriber : public rclcpp::Node {
 
  private:
   /**
-   * @brief topic_callback function, calls the topic and fetches the message from it
-   * 
-   * @param msg 
+   * @brief topic_callback function, calls the topic and fetches the message
+   * from it
+   *
+   * @param msg
    */
   void topic_callback(const std_msgs::msg::String& msg) const {
     RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
@@ -59,10 +60,10 @@ class MinimalSubscriber : public rclcpp::Node {
 
 /**
  * @brief main function
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
